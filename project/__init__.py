277c5321284100969f02260065efd41c44e741ca
Anarbb <anarbaoui@gmail.com>
2021-07-02 20:44:34 +0100

    main

--- project/__init__.py
+++ project/__init__.py
@@ -28,11 +28,9 @@ def root():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    # note that we set the 404 status explicitly
     return render_template("404.html"), 404
 
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    # note that we set the 500 status explicitly
     return render_template("500.html"), 500
